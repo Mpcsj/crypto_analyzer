18f264b9b5969d5e110d0e461b12f04589cc6978
Marcos Paulo <marcosjunioribm@gmail.com>
2021-12-26 12:13:43 -0300

    - :truck: move "save_from_df" to BaseRepository

--- crypto_analyzer/data_access/db/base_repository.py
+++ crypto_analyzer/data_access/db/base_repository.py
@@ -1,13 +1,27 @@
-from crypto_analyzer.data_access.db.db_connector import DbConnection
 from abc import ABC, abstractmethod
+
+import pandas
+import sqlalchemy.exc
+from airflow.utils import sqlalchemy
+
+from crypto_analyzer.data_access.db.db_connector import DbConnection
 
 
 class BaseRepository(ABC):
     _instance = None
+    _collection_name = None
 
     def __init__(self):
         self.connection = DbConnection.get_instance()
         self.connection.execute_command(self._get_create_db_if_not_exists_str())
+
+    def save_from_df(self, data: pandas.DataFrame):
+        try:
+            data.to_sql(self._collection_name, con=self.connection.engine, if_exists='append', index=False)
+        except sqlalchemy.exc.IntegrityError as err:
+            print('Integrity error >>', err)
+        except Exception as err:
+            print(err)
 
     @abstractmethod
     def _get_create_db_if_not_exists_str(self) -> str:
--- crypto_analyzer/data_access/db/coin_price_repository.py
+++ crypto_analyzer/data_access/db/coin_price_repository.py
@@ -1,10 +1,9 @@
-import pandas
-import sqlalchemy.exc
-
 from crypto_analyzer.data_access.db.base_repository import BaseRepository
 
 
 class CoinPriceRepository(BaseRepository):
+    _collection_name = 'coin_price'
+
     def _get_create_db_if_not_exists_str(self) -> str:
         return """
         CREATE TABLE IF NOT EXISTS coin_price(
@@ -15,14 +14,6 @@ class CoinPriceRepository(BaseRepository):
             UNIQUE(coin_name,updated_at)
         );
         """
-
-    def save_from_df(self, data: pandas.DataFrame):
-        try:
-            data.to_sql('coin_price', con=self.connection.engine, if_exists='append', index=False)
-        except sqlalchemy.exc.IntegrityError as err:
-            print('Integrity error >>', err)
-        except Exception as err:
-            print(err)
 
     @classmethod
     def get_instance(cls) -> 'CoinPriceRepository':
